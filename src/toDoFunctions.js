fac1f9106077caa1762a417d94e432c54446b2bd
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-17 14:36:43 +0300

    added delete button functionality for each to-do item + refactored code with helper functions for better abstraction

--- src/toDoFunctions.js
+++ src/toDoFunctions.js
@@ -2,15 +2,10 @@
 import ToDo from "/src/classes";
 import "../src/style.css";
 
-export const toDoList = [];
+/* ------------HELPER FUNCTIONS------------------ */
+/* ---------------------------------------------- */
 
-export function createToDo(title, dueDate, text) {
-  const toDo = new ToDo(title, dueDate, text);
-  toDoList.push(toDo);
-}
-
-export function displayToDo(toDo) {
-  const notesGrid = document.getElementById("notesGrid");
+function createToDoElements(toDo) {
   const toDoItem = document.createElement("div");
   const toDoTitle = document.createElement("h3");
   const deadlineBox = document.createElement("div");
@@ -20,12 +15,13 @@ export function displayToDo(toDo) {
   const deleteBtn = document.createElement("button");
 
   toDoItem.classList.add("notes__grid--item", "flex-col");
+  toDoItem.setAttribute("id", `item${toDo.key}`);
   toDoTitle.classList.add("notes__grid--item-title");
   duePara.classList.add("notes__grid--item-para");
   doneBtn.classList.add("doneBtn");
-  doneBtn.setAttribute("id", "done");
+  doneBtn.setAttribute("id", `done${toDo.key}`);
   deleteBtn.classList.add("deleteBtn");
-  deleteBtn.setAttribute("id", "delete");
+  deleteBtn.setAttribute("id", `del${toDo.key}`);
 
   toDoTitle.textContent = toDo.title;
   duePara.textContent = "Due:";
@@ -37,10 +33,11 @@ export function displayToDo(toDo) {
   toDoItem.append(toDoTitle, deadlineBox, doneBtn, deleteBtn);
   deadlineBox.append(duePara, dueSpan);
   notesGrid.appendChild(toDoItem);
+
+  return document.getElementById(`item${toDo.key}`);
 }
 
-export function expandToDo(toDo) {
-  const notesGrid = document.getElementById("notesGrid");
+function createExpandedToDoElements(toDo) {
   const toDoWrapper = document.createElement("div");
   const toDoContent = document.createElement("div");
   const toDoTitle = document.createElement("h3");
@@ -57,23 +54,40 @@ export function expandToDo(toDo) {
   notesGrid.appendChild(toDoWrapper);
 }
 
+/* ---------------------------------------------- */
+/* ---------------------------------------------- */
+
+const notesGrid = document.getElementById("notesGrid");
+export const toDoList = [];
+
+export function createToDo(title, dueDate, text) {
+  const toDo = new ToDo(title, dueDate, text);
+  toDoList.push(toDo);
+}
+
+export function displayToDo(toDo) {
+  const toDoItem = createToDoElements(toDo);
+  document.getElementById(`del${toDo.key}`).addEventListener("click", (e) => {
+    notesGrid.removeChild(toDoItem);
+    toDo.deleteItem(toDo.key);
+    console.log(toDoList);
+  });
+}
+
 export function expandClickedItems() {
   const allToDos = document.querySelectorAll(".notes__grid--item");
-  const notesGrid = document.getElementById("notesGrid");
+  const toDoArray = Array.from(allToDos);
 
-  allToDos.forEach((toDo, index) => {
-    toDo.setAttribute("id", `${index}`);
-    toDo.addEventListener("click", () => {
-      // console.log(toDo.getAttribute("id"));
-      expandToDo(toDoList[index]);
-      const wrapper = document.querySelector(".to-do-wrapper");
-      wrapper.addEventListener("click", () => {
-        notesGrid.removeChild(wrapper);
-      });
+  toDoArray.forEach((toDo, index) => {
+    const item = document.getElementById(`item${index}`);
+    item.addEventListener("click", (e) => {
+      if (e.target.nodeName.toLowerCase() !== "button") {
+        createExpandedToDoElements(toDoList[index]);
+        const wrapper = document.querySelector(".to-do-wrapper");
+        wrapper.addEventListener("click", () => {
+          notesGrid.removeChild(wrapper);
+        });
+      } else return;
     });
   });
-
-  function deleteToDo(toDo) {
-    // const deleteBtn = document.ATTRIBUTE_NODE;
-  }
 }
