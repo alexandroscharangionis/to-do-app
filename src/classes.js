3c7743c2de4b2b7d8037421dd5c299e93452b296
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-17 14:35:13 +0300

    added method to delete object instance

--- src/classes.js
+++ src/classes.js
@@ -1,10 +1,19 @@
 "use strict";
 
+import { toDoList } from "./toDoFunctions";
+
 export default class ToDo {
+  static lastKey = 0;
+  key;
   constructor(title, dueDate, text) {
     this.title = title;
     this.dueDate = dueDate;
     this.text = text;
     this.done = false;
+    this.key = ToDo.lastKey++;
+  }
+
+  deleteItem(index) {
+    toDoList.splice(index, 1);
   }
 }
