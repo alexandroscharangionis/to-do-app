958d4ff24f45b4ff7eadb4a9c92496317ff0cde1
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-18 11:24:11 +0300

    moved functions for DOM element creation into separate module

--- src/toDoFunctions.js
+++ src/toDoFunctions.js
@@ -1,67 +1,10 @@
 "use strict";
 import ToDo from "/src/classes";
 import "../src/style.css";
-
-/* ------------HELPER FUNCTIONS------------------ */
-/* ---------------------------------------------- */
-
-// Creates DOM elements, adds CSS styles, appends them to DOM, return reference to main to-do item"
-function createToDoElements(toDo) {
-  const toDoItem = document.createElement("div");
-  const toDoTitle = document.createElement("h3");
-  const deadlineBox = document.createElement("div");
-  const duePara = document.createElement("p");
-  const dueSpan = document.createElement("span");
-  const doneBtn = document.createElement("button");
-  const deleteBtn = document.createElement("button");
-
-  toDoItem.classList.add("notes__grid--item", "flex-col");
-  toDoItem.setAttribute("id", `item${toDo.key}`);
-  toDoTitle.classList.add("notes__grid--item-title");
-  duePara.classList.add("notes__grid--item-para");
-  doneBtn.classList.add("doneBtn");
-  doneBtn.setAttribute("id", `done${toDo.key}`);
-  deleteBtn.classList.add("deleteBtn");
-  deleteBtn.setAttribute("id", `del${toDo.key}`);
-
-  toDoTitle.textContent = toDo.title;
-  duePara.textContent = "Due:";
-  dueSpan.textContent = toDo.dueDate;
-  doneBtn.innerText = "done?";
-  deleteBtn.textContent = "delete";
-
-  notesGrid.appendChild(toDoItem);
-  toDoItem.append(toDoTitle, deadlineBox, doneBtn, deleteBtn);
-  deadlineBox.append(duePara, dueSpan);
-  notesGrid.appendChild(toDoItem);
-
-  return [
-    document.getElementById(`item${toDo.key}`),
-    document.getElementById(`del${toDo.key}`),
-    document.getElementById(`done${toDo.key}`),
-  ];
-}
-
-// Creates DOM elements, adds CSS styles, appends them to DOM
-function createExpandedToDoElements(toDo) {
-  const toDoWrapper = document.createElement("div");
-  const toDoContent = document.createElement("div");
-  const toDoTitle = document.createElement("h3");
-  const toDoText = document.createElement("p");
-
-  toDoWrapper.classList.add("to-do-wrapper");
-  toDoContent.classList.add("to-do-content", "flex-col");
-  toDoTitle.classList.add("notes__grid--item-title");
-  toDoTitle.textContent = toDo.title;
-  toDoText.textContent = toDo.text;
-
-  toDoWrapper.appendChild(toDoContent);
-  toDoContent.append(toDoTitle, toDoText);
-  notesGrid.appendChild(toDoWrapper);
-}
-
-/* ---------------------------------------------- */
-/* ---------------------------------------------- */
+import {
+  createToDoElements,
+  createExpandedToDoElements,
+} from "/src/generate_DOM_elements";
 
 const notesGrid = document.getElementById("notesGrid");
 export const toDoList = [];
@@ -75,7 +18,7 @@ export function createToDo(title, dueDate, text) {
 // Generates HTML & CSS for each element, adds event listeners for 'delete' and 'done'
 export function displayToDo(toDo) {
   const [toDoItem, delBtn, doneBtn] = createToDoElements(toDo);
-  delBtn.addEventListener("click", (e) => {
+  delBtn.addEventListener("click", () => {
     notesGrid.removeChild(toDoItem);
     toDo.deleteItem(toDo.key);
   });
