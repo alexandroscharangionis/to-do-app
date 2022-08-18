e70617d84f764dcde825be68803a59b64150d3b9
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-18 19:23:04 +0300

    no changes, just testing

--- src/index.js
+++ src/index.js
@@ -9,27 +9,5 @@ import {
 import "../src/style.css";
 import { createForm } from "/src/generate_DOM_elements";
 
-createToDo(
-  "Clean house",
-  "today 5pm",
-  "Vacuum all rooms. Dust shelves. Mop all the floors. Thoroughly clean bathroom."
-);
-createToDo(
-  "Learn coding",
-  "thursday 2pm",
-  "Learn functions and variables. Practice arrays and objects. Run some loops. Solve bugs."
-);
-createToDo(
-  "Make food",
-  "monday 8am",
-  "Make breakfast. Prepare dinner. Cook lunch. Store it for later."
-);
-
-for (let i = 0; i < toDoList.length; i++) {
-  displayToDo(toDoList[i]);
-}
-
-expandClickedItems();
-
 const newBtn = document.getElementById("createNew");
 newBtn.addEventListener("click", createForm);
