6a2984d5b989962f4d3dc9b297c3662bb8c10502
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-22 19:06:04 +0300

    added event listener for 'piority' btn

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ import {
 } from "/src/toDoFunctions";
 import "../src/style.css";
 import { createForm } from "/src/generate_DOM_elements";
-import { sortByActive } from "./sorting";
+import { sortByActive, sortByPriority } from "./sorting";
 
 const newBtn = document.getElementById("createNew");
 newBtn.addEventListener("click", createForm);
 
 const active = document.getElementById("active");
 active.addEventListener("click", sortByActive);
+
+const priority = document.getElementById("priority");
+priority.addEventListener("click", sortByPriority);
