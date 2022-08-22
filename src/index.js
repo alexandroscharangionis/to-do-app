174f2098ee6c9a45b5ac47bee0bab755caa944a5
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-22 17:57:31 +0300

    added event listener for 'active' sorting btn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import {
 } from "/src/toDoFunctions";
 import "../src/style.css";
 import { createForm } from "/src/generate_DOM_elements";
+import { sortByActive } from "./sorting";
 
 const newBtn = document.getElementById("createNew");
 newBtn.addEventListener("click", createForm);
+
+const active = document.getElementById("active");
+active.addEventListener("click", sortByActive);
