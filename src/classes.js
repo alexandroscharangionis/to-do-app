f0b805d4d079e7f9ff1943cf618d54339c49b40f
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-23 13:37:28 +0300

    added constructor property for project assignment

--- src/classes.js
+++ src/classes.js
@@ -4,12 +4,13 @@ import { toDoList } from "./toDoFunctions";
 
 export default class ToDo {
   static keyCount = 0;
-  constructor(title, dueDate, text) {
+  constructor(title, project, dueDate, text) {
     this.title = title;
     this.dueDate = dueDate;
     this.text = text;
     this.done = false;
     this.key = ToDo.keyCount++;
+    this.project = project;
   }
 
   deleteItem(item) {
