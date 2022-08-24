c6ab09e4757cc9d1445d42b1d582853ba49af6ff
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-24 16:12:54 +0300

    refactored event listener for project title so that if current project's title get's renamed, it also gets renamed inside projects array

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 "use strict";
 
 import {
-  projects,
   toDoList,
   createToDo,
   displayToDo,
@@ -10,21 +9,31 @@ import {
 import "../src/style.css";
 import { createForm } from "/src/generate_DOM_elements";
 import { sortByActive, sortByPriority } from "./sorting";
+import { Project } from "./classes";
+import { projects, currentProject, createProject } from "./projectsFunctions";
 
-let currentProject = projects[0];
+createProject("My Project");
+// console.log(projects);
+// console.log(currentProject);
+// console.log(currentProject.key);
 
 const newBtn = document.getElementById("createNew");
 newBtn.addEventListener("click", createForm);
 
 const projectTitle = document.getElementById("projectTitle");
-projectTitle.textContent = currentProject.name;
+projectTitle.textContent = currentProject.title;
 
+// If user edits current project's title, update it into projects array too (and update currentObject variable so it displays correctly)
 projectTitle.addEventListener("blur", () => {
   if (projectTitle.textContent === "") {
-    projectTitle.textContent = currentProject.name;
+    projectTitle.textContent = currentProject.title;
   } else {
-    currentProject.name = projectTitle.textContent;
-    console.log(currentProject.name);
+    for (let i = 0; i < projects.length; i++) {
+      if (projects[i].key === currentProject.key) {
+        projects[i].title = projectTitle.textContent;
+        currentProject = projects[i];
+      }
+    }
   }
 });
 
