986d599816f9010ffd75358d8e9fb52341e01d0f
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-25 17:11:23 +0300

    created displayProjects function that loops through projects array and displays every time with help of createProjectItem function

---
+++ src/projectFunctions.js
@@ -0,0 +1,30 @@
+"use strict";
+import { Project } from "./classes";
+
+export const projects = [];
+export let currentProject;
+
+export function createProject(title) {
+  const project = new Project(title);
+  projects.push(project);
+  currentProject = project;
+  return project;
+}
+
+export function displayProjects() {
+  const notesGrid = document.getElementById("notesGrid");
+  const allItems = document.querySelectorAll(".notes__grid--item");
+  allItems.forEach((item) => {
+    notesGrid.removeChild(item);
+  });
+
+  for (const project in projects) {
+    const [projectItem, delBtn] = createProjectItem(project);
+    delBtn.addEventListener("click", () => {
+      // Removes item from display
+      notesGrid.removeChild(projectItem);
+      // Removes item from project array also
+      project.deleteItem(project);
+    });
+  }
+}
