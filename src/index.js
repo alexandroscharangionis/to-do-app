234e3b069fe1c95c346267e1228d9b08ea647b57
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-25 19:07:06 +0300

    refactored code to use imported functions for better code readability

--- src/index.js
+++ src/index.js
@@ -1,44 +1,25 @@
 "use strict";
 
-import {
-  toDoList,
-  createToDo,
-  displayToDo,
-  expandClickedItems,
-} from "/src/toDoFunctions";
 import "../src/style.css";
-import { createForm } from "/src/generate_DOM_elements";
+import { createForm } from "/src/DOM_element_creation";
 import { sortByActive, sortByPriority } from "./sorting";
-import { Project } from "./classes";
-import { projects, currentProject, createProject } from "./projectsFunctions";
+import {
+  currentProject,
+  createProject,
+  displayProjects,
+  projectTitle,
+  editProjectTitle,
+} from "./projectFunctions";
 
 createProject("My Project");
-// console.log(projects);
-// console.log(currentProject);
-// console.log(currentProject.key);
-
-const newBtn = document.getElementById("createNew");
-newBtn.addEventListener("click", createForm);
-
-const projectTitle = document.getElementById("projectTitle");
 projectTitle.textContent = currentProject.title;
-
-// If user edits current project's title, update it into projects array too (and update currentObject variable so it displays correctly)
-projectTitle.addEventListener("blur", () => {
-  if (projectTitle.textContent === "") {
-    projectTitle.textContent = currentProject.title;
-  } else {
-    for (let i = 0; i < projects.length; i++) {
-      if (projects[i].key === currentProject.key) {
-        projects[i].title = projectTitle.textContent;
-        currentProject = projects[i];
-      }
-    }
-  }
-});
-
+const projectsBtn = document.getElementById("projectsBtn");
+const newBtn = document.getElementById("createNew");
 const active = document.getElementById("active");
-active.addEventListener("click", sortByActive);
-
 const priority = document.getElementById("priority");
+
+projectsBtn.addEventListener("click", displayProjects);
+newBtn.addEventListener("click", createForm);
+projectTitle.addEventListener("blur", editProjectTitle);
+active.addEventListener("click", sortByActive);
 priority.addEventListener("click", sortByPriority);
