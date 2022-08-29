835ae0d7df451200aa836eb89bab518a15482d38
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-29 17:21:35 +0300

    added event listener and functionality for all todos button

--- src/index.js
+++ src/index.js
@@ -10,15 +10,18 @@ import {
   projectTitle,
   editProjectTitle,
 } from "./projectFunctions";
+import { displayAllTodos } from "./toDoFunctions";
 
 createProject("My Project");
 projectTitle.textContent = currentProject.title;
 const projectsBtn = document.getElementById("projectsBtn");
+const allTodosBtn = document.getElementById("allTodosBtn");
 const newBtn = document.getElementById("createNew");
 const active = document.getElementById("active");
 const priority = document.getElementById("priority");
 
 projectsBtn.addEventListener("click", displayProjects);
+allTodosBtn.addEventListener("click", displayAllTodos);
 newBtn.addEventListener("click", createForm);
 projectTitle.addEventListener("blur", editProjectTitle);
 active.addEventListener("click", sortByActive);
