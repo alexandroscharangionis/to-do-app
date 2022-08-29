c4c6cba5c3c5032fb9151c43e2540b765afbf128
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-29 14:21:35 +0300

    created openProjectForFormIntake function

--- src/projectFunctions.js
+++ src/projectFunctions.js
@@ -4,8 +4,10 @@ import {
   createProjectItem,
   clearNotesGrid,
   createExpandedToDoElements,
+  changeSidebarContentAllProjects,
+  changeSidebarContentProject,
 } from "./DOM_element_creation";
-import { displayToDo, toDoList } from "./toDoFunctions";
+import { displayToDo, toDoList, notesGrid } from "./toDoFunctions";
 
 export const projects = [];
 export let currentProject;
@@ -22,7 +24,7 @@ export function createProject(title) {
 // Clear current grid, loop through projects array, create DOM elements for every item, add event listeners for delete button and for opening clicked project
 export function displayProjects() {
   clearNotesGrid();
-
+  changeSidebarContentAllProjects();
   for (let i = 0; i < projects.length; i++) {
     const [projectItem, delBtn] = createProjectItem(projects[i]);
     delBtn.addEventListener("click", () => {
@@ -39,7 +41,7 @@ export function displayProjects() {
 // Displays to-do items corresponding to that project
 function openProject(clicked) {
   clearNotesGrid();
-
+  changeSidebarContentProject();
   // Loops through projects array to find clicked project based on key and set it as currentProject
   for (let i = 0; i < projects.length; i++) {
     if (projects[i].key === clicked.target.id.slice(7)) {
@@ -47,6 +49,35 @@ function openProject(clicked) {
     }
   }
   projectTitle.textContent = currentProject.title;
+  // Loops through toDoList array to find every item that has the clicked project assigned to it, then displays every matching item
+  for (let i = 0; i < toDoList.length; i++) {
+    if (toDoList[i].project === currentProject.title) {
+      const [itemReference, itemObj] = displayToDo(toDoList[i]);
+
+      itemReference.addEventListener("click", (e) => {
+        if (e.target.nodeName.toLowerCase() !== "button") {
+          createExpandedToDoElements(itemObj);
+          const wrapper = document.querySelector(".to-do-wrapper");
+          wrapper.addEventListener("click", (e) => {
+            if (e.target === wrapper) {
+              notesGrid.removeChild(wrapper);
+            } else return;
+          });
+        } else return;
+      });
+    }
+  }
+}
+
+export function openProjectForFormIntake() {
+  clearNotesGrid();
+  // Loops through projects array to find form chosen project based on title and set it as currentProject
+  for (let i = 0; i < projects.length; i++) {
+    if (projects[i].title === document.getElementById("todo__project").value) {
+      currentProject = projects[i];
+    }
+  }
+  changeSidebarContentProject();
   // Loops through toDoList array to find every item that has the clicked project assigned to it, then displays every matching item
   for (let i = 0; i < toDoList.length; i++) {
     if (toDoList[i].project === currentProject.title) {
