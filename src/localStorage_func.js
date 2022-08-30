f84d01dfa8ea82fb88112cfc3ad7e0924b8f5653
alexandroscharangionis <alexandroscharangionis@gmail.com>
2022-08-30 17:29:15 +0300

    Initial dist subtree commit

--- src/localStorage_func.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-import { toDoList } from "./toDoFunctions";
-import { projects } from "./projectFunctions";
-
-export function saveToLocal() {
-  localStorage.setItem("todoList", JSON.stringify(toDoList));
-  localStorage.setItem("projectList", JSON.stringify(projects));
-}
